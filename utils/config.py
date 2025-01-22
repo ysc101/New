d34865703713a889592cb691d0b99e36fc1ab996
ysc101 <yogesh.chavhan@prmgroup.in>
2025-01-22 12:59:17 +0530

    New Programs 21012025

--- utils/config.py
+++ utils/config.py
@@ -2,4 +2,4 @@ class Config:
     BASE_URL = "https://ps.demofms.com/Login.aspx"  # Replace with your actual login URL
     USERNAME = "MAKERWORK"
     PASSWORD = "Pass@123"
-    DRIVER_PATH = r"C:\Laptop_Backup\AUTOMATION\Drivers\chromedriver.exe"
+    DRIVER_PATH = r"E:\Yogesh_Desktop_Backup\Deskstop\AUTOMATION\chromedriver.exe"
--- utils/driver_setup.py
+++ utils/driver_setup.py
@@ -1,6 +1,8 @@
 # utils/driver_setup.py
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 from utils.config import Config
 
 def get_driver():
@@ -8,4 +10,8 @@ def get_driver():
     driver = webdriver.Chrome(service=service)
     driver.implicitly_wait(50)
     driver.maximize_window()
+    return driver
+
+def get_driver():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     return driver
